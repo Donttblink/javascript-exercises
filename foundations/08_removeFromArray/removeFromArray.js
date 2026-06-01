e4c60bb3e69c97231e94941f7067e18d6866bd50
Donttblink <jonathon.lerose@gmail.com>
2026-06-01 08:08:21 -0400

    rough

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function(arr, remove) {
-  return arr.filter(item => item !== remove);
-
+const removeFromArray = function(arr, ...items) {
+  const newArray = [];
+  arr.forEach((item) => {
+    if (!items.includes(item)) {
+      newArray.push(item);
+    }
+    });
+    return newArray;
+  
 };
 
 // Do not edit below this line
