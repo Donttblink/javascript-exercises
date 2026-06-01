642620ab8371db672906900fd7e241add54b16b8
Donttblink <jonathon.lerose@gmail.com>
2026-06-01 12:08:29 -0400

    added a variable for total and a loop function to add sum from min to max and add to total

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+  let total = 0
+  for (let i = min ; i >= max; i++){
+    total =+ i;  
+  }
+  return total;
+
+
+  //1. declare a variable to hol the running total, start it at 0
+  //2. loop with i from min to max (inclusive)
+  //3. inside the loop, add i to the total
+  //4. after the loop, return the total
+
 
 };
 
