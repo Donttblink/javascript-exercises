56542b28c6f8c7b692536e9bdcf5902831694301
Donttblink <jonathon.lerose@gmail.com>
2026-06-01 18:47:39 -0400

    used two constants and math.floor to stop non integer and set params for leap year

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  const isLeap = true;
+  const notLeap = false;
+  if (Math.floor(year) !== year) return 'ERROR';
+  if (year / 4 && year % 4 == 0 || year % 400 == 0) {
+    return isLeap;
+  }else {
+    return notLeap;
+  }
 
-};
+  };
+
+
+
+  
+  //input year
+  //check if year is leap year (divisible by 4 and not divisible by 100 unless divisible by 400)
+  //if is a leap year return true
+  //not a leap year return false
+
 
 // Do not edit below this line
 module.exports = leapYears;
