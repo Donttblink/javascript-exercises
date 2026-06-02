d882a13056605b8ecfe0ea2a6fed7ea69dc7355c
Donttblink <jonathon.lerose@gmail.com>
2026-06-02 00:04:18 -0400

    completed solo baby

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(fahrenheit) {
+  let converted = (fahrenheit - 32) * (5/9);
+  return Math.round(converted *10) / 10;
 
-const convertToFahrenheit = function() {
-};
 
+};
+// convert number from fahrenheit to celsius
+//use the equation xF =(x-32)*5/9 C
+
+const convertToFahrenheit = function(celsius) {
+  let converted =(celsius*(9/5)+32);
+  return Math.round(converted *10) / 10;
+
+};
+//convert number from celsius to fahrenheit
+//use the equation xC= (x*9/5+32)F
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
