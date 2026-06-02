e680775c79b414b3ac9e5dbf7ee1b5b6829f8b2d
Donttblink <jonathon.lerose@gmail.com>
2026-06-01 23:29:11 -0400

    fixed the truthy loop

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -2,17 +2,14 @@ const leapYears = function(year) {
   const isLeap = true;
   const notLeap = false;
   if (Math.floor(year) !== year) return 'ERROR';
-  if (year / 4 && year % 4 == 0 || year % 400 == 0) {
+  if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
     return isLeap;
-  }else {
+    }else {
     return notLeap;
   }
 
   };
-
-
-
-  
+ 
   //input year
   //check if year is leap year (divisible by 4 and not divisible by 100 unless divisible by 400)
   //if is a leap year return true
