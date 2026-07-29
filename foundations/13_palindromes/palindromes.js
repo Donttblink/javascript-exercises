40f3c9a8b3571d6f1d92e38e825d6d05e960428d
Donttblink <jonathon.lerose@gmail.com>
2026-07-29 19:30:55 -0400

    Completed palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,9 +1,13 @@
 const palindromes = function (str) {
-  let cleanString = '';
+  let cleanString = "";
   for (let i = 0; i < str.length; i++) {
     const c = str[i].toLowerCase();
-  }
-
+    if ((c >= "a" && c <= "z") || (c >= "0" && c <= "9")) {
+      cleanString += c;
+    }
+  };
+  let reversed = cleanString.split('').reverse().join('');
+  return cleanString === reversed;
 };
 
 // Do not edit below this line
