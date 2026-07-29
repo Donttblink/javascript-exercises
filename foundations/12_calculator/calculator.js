75f2d269ad4f017802f619e16c07503d0b7866bb
Donttblink <jonathon.lerose@gmail.com>
2026-07-29 09:52:28 -0400

    completed calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  return arr.reduce((sum, current) => sum + current, 0);
+};
+
+const multiply = function(arr) {
+return arr.reduce((sum, current) => sum  * current);
+};
+
+const power = function(base, power) {
+  return base ** power;
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+	return product;
 };
 
 // Do not edit below this line
