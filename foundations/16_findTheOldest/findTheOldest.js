ae131dfb0cbf749c6d0f5a9624dd67763d29b30a
Donttblink <jonathon.lerose@gmail.com>
2026-07-30 09:51:11 -0400

    completed exercises

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const oldest = people.reduce((a, b) => {
+    const now = new Date().getFullYear();
+    if (a.yearOfDeath === undefined) {
+      a.yearOfDeath = now;
+    };
+    if (b.yearOfDeath === undefined) {
+      b.yearOfDeath = now;
+    }
+    const lastGuy = a.yearOfDeath - a.yearOfBirth;
+    const nextGuy = b.yearOfDeath - b.yearOfBirth;
+    return lastGuy > nextGuy ? a : b;
+  });
+  return oldest;
 };
 
 // Do not edit below this line
