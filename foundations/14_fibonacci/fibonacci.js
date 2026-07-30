29d3ce1dfcf693c06d56990d4d91857f2e921a00
Donttblink <jonathon.lerose@gmail.com>
2026-07-29 22:06:19 -0400

    completed fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let count;
+  if (typeof n !== "number") {
+    count = parseInt(n);
+  } else {
+    count = n;
+  }
+  if (count === 0) {
+    return 0;
+  }
+  if (count < 0) {
+    return "OOPS";
+  }
 
+  let firstPrev = 1;
+  let secondPrev = 0;
+  for (let i = 2; i <= n; i++) {
+    let current = firstPrev + secondPrev;
+    secondPrev = firstPrev;
+    firstPrev = current;
+  }
+  return firstPrev;
 };
 
 // Do not edit below this line
